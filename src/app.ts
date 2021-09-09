43da704c23e1252246af74a69de09dc325b30da3
vinibispo <vini.bispo015@gmail.com>
2021-09-08 22:02:58 -0300

    feat: update express js and basic config to use routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
-import * as express from "express";
+import express from "express";
 import router from "./routes";
+import "./database";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", router);
 export default app;
