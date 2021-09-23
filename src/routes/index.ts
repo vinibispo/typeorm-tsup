8e3cc7e79e228d78a903d5265472c22aca0372fc
vinibispo <vini.bispo015@gmail.com>
2021-09-22 22:18:38 -0300

    feat: add new routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 
 import userRouter from "../routes/user_router";
+import sessionRouter from "../routes/session_router";
 const router = Router();
 
 router.use("/users", userRouter);
+router.use(`/users/:user_id/sessions`, sessionRouter);
 
 export default router;
